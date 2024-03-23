1601076985c1c4483c0f7e1cf182580a1599febf
Jair <jair.terrones429@gmail.com>
2024-03-23 02:54:55 -0500

    Type of date

--- manage.py
+++ manage.py
@@ -1,3 +1,9 @@
 """World Cup"""
 
 print('Hello World')
+
+print(type('World Cup'))  # Tipo 'srt'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(1 + 2j))  # Tipo 'complex number'
+print(type(True))  # Tipo 'bool'
